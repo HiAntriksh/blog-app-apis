61c67886eb3c6a90fcc3f0a908761c847ab2e70b
HiAntriksh <antrikshspacex@yahoo.com>
2025-05-15 16:25:56 +0530

    commit
    git push

--- src/main/java/com/codewithantriksh/blog/BlogAppApisApplication.java
+++ src/main/java/com/codewithantriksh/blog/BlogAppApisApplication.java
@@ -1,7 +1,14 @@
 package com.codewithantriksh.blog;
 
+<<<<<<< HEAD
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+=======
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+>>>>>>> 6922f9c (commit)
 
 @SpringBootApplication
 public class BlogAppApisApplication {
@@ -10,4 +17,14 @@ public class BlogAppApisApplication {
 		SpringApplication.run(BlogAppApisApplication.class, args);
 	}
 
+<<<<<<< HEAD
 }
+=======
+	@Bean
+	public ModelMapper modelMapper() {
+	return  new ModelMapper();
+	
+}
+
+}
+>>>>>>> 6922f9c (commit)
--- src/test/java/com/codewithantriksh/blog/BlogAppApisApplicationTests.java
+++ src/test/java/com/codewithantriksh/blog/BlogAppApisApplicationTests.java
@@ -1,13 +1,42 @@
 package com.codewithantriksh.blog;
 
 import org.junit.jupiter.api.Test;
+<<<<<<< HEAD
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogAppApisApplicationTests {
 
+=======
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codewithantriksh.blog.repositories.UserRepo;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Autowired
+	private UserRepo userRepo;
+	
+>>>>>>> 6922f9c (commit)
 	@Test
 	void contextLoads() {
 	}
 
+<<<<<<< HEAD
 }
+=======
+    @Test
+	public void repoTest()
+	{
+	 String className = this.userRepo.getClass().getName();
+	 String packName = this.userRepo.getClass().getPackageName();
+	 System.out.println(className);
+	 System.out.println(packName);
+	}
+	
+
+
+}
+>>>>>>> 6922f9c (commit)
